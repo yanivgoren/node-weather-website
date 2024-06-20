ea61511cdac9ad5a6a1d06a1d14be78d18b4d976
Yaniv Goren <Yaniv.Goren@audiocodes.com>
2024-06-20 11:01:32 +0300

    Add umidity data to forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,6 +6,7 @@ const forecast = (latitude, longitude, callback) => {
    
     //const url = 'https://api.tomorrow.io/v4/weather/realtime?location=' + encodeURIComponent(address) + '&apikey=wSypn4SXa5A8ISDydYMePYQJMKi4XuZd'
     const url = 'https://api.tomorrow.io/v4/weather/realtime?location=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&apikey=wSypn4SXa5A8ISDydYMePYQJMKi4XuZd'
+    console.log(url)
 
     //request({ url: url, json: true }, (error, response) => {
     request({ url, json: true }, (error, {body}) => {
@@ -23,7 +24,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(undefined, 
                 //`It's currently ${data.data.values['temperature']} degress out in ${data.location['name']}. There is a ${data.data.values['precipitationProbability']}% chance of rain.`
                 //`It's currently ${data.data.values['temperature']} degress out. There is a ${data.data.values['precipitationProbability']}% chance of rain.`
-                `It's currently ${body.data.values['temperature']} degress out. There is a ${body.data.values['precipitationProbability']}% chance of rain.`
+                `It's currently ${body.data.values['temperature']} degress out. There is a ${body.data.values['precipitationProbability']}% chance of rain and ${body.data.values['humidity']}% humidity`
             )
         }
     })
